2fee7c30742f6d837a1b6ddcb6fe4b09fe4079bc
TR-HarshVVVV <harshkrish2323@gmail.com>
2025-10-20 00:46:50 +0530

    Add Composer autoloader and metadata files
    Adds Composer-generated autoloader files and metadata to the vendor directory, including class loader, installed versions, and platform check scripts. This is typically the result of running 'composer install' to set up PHP autoloading and dependency management.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '15b8bb14943a0b154a7f7af25d65448735f96e85',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '15b8bb14943a0b154a7f7af25d65448735f96e85',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
